482702aa02fd3994100f7d76d5c416ffabcbdec0
sameer-gits <ms3054825@gmail.com>
2024-04-22 22:01:57 +0530

    added binary search with test

---
+++ binary_search/binary_search.ts
@@ -0,0 +1,20 @@
+export default function binary_search(haystack: number[], needle: number): boolean {
+
+    let low = 0;
+    let high = haystack.length;
+
+    do {
+        const mid = Math.floor(low + (high - low) / 2);
+        const value = haystack[mid];
+
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    } while (low < high);
+
+    return false;
+}
