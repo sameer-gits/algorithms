5029ac9d9cb178e4f82c4abf9132e21310223066
sameer-gits <ms3054825@gmail.com>
2024-04-30 01:21:44 +0530

    added ring_buffer but failing test

---
+++ ring_buffer/ring_buffer.ts
@@ -0,0 +1,34 @@
+export default class ring_buffer<T> {
+    private buffer: (T | undefined)[];
+    private tail: number;
+    private head: number;
+
+    constructor() {
+        this.head = 0;
+        this.tail = 0;
+        this.buffer = new Array<T>(5);
+    }
+
+    push(item: T): (T | undefined)[] {
+        //if(this.tail === this.head) {
+        // TODO: add a while loop for making a new bigger array;
+        //}
+        const tail = this.tail % this.buffer.length;
+        this.buffer[tail] = item;
+        this.tail++;
+        return this.buffer;
+    }
+
+    pop(): T | undefined {
+        const head = this.head & this.buffer.length;
+        const deleted_value = this.buffer[head];
+        this.buffer[head] = undefined;
+        this.head++;
+        return deleted_value; 
+    }
+
+
+    get(index: number): T | undefined {
+        return this.buffer[index];
+    }
+}
