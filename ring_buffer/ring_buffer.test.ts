76996b42d1bf96a900f4dd53d5262d762ea18ea0
sameer-gits <ms3054825@gmail.com>
2024-04-30 22:20:30 +0530

    now all tests are passed and i just have to implement loop for increasing capacity

--- ring_buffer/ring_buffer.test.ts
+++ ring_buffer/ring_buffer.test.ts
@@ -3,8 +3,11 @@ import RingBuffer from "./ring_buffer"
 test("RingBuffer", function() {
     const buffer = new RingBuffer<number>();
 
-
-
+    expect(buffer.pop()).toEqual(undefined);
+    expect(buffer.pop()).toEqual(undefined);
+    expect(buffer.pop()).toEqual(undefined);
+    expect(buffer.pop()).toEqual(undefined);
+    expect(buffer.pop()).toEqual(undefined);
     buffer.push(5);
     expect(buffer.pop()).toEqual(5);
     expect(buffer.pop()).toEqual(undefined);
@@ -14,6 +17,16 @@ test("RingBuffer", function() {
     expect(buffer.pop()).toEqual(42);
     expect(buffer.pop()).toEqual(9);
     expect(buffer.pop()).toEqual(undefined);
+    buffer.push(5);
+    buffer.push(5);
+    buffer.push(5);
+    expect(buffer.pop()).toEqual(5);
+    expect(buffer.pop()).toEqual(5);
+    expect(buffer.pop()).toEqual(5);
+    expect(buffer.pop()).toEqual(undefined);
+    expect(buffer.pop()).toEqual(undefined);
+    expect(buffer.pop()).toEqual(undefined);
+    expect(buffer.pop()).toEqual(undefined);
 
     buffer.push(42);
     buffer.push(9);
@@ -21,4 +34,11 @@ test("RingBuffer", function() {
     expect(buffer.get(2)).toEqual(12);
     expect(buffer.get(1)).toEqual(9);
     expect(buffer.get(0)).toEqual(42);
+    buffer.push(10);
+    expect(buffer.get(0)).toEqual(10);
+    buffer.push(0);
+    expect(buffer.get(1)).toEqual(0);
+    expect(buffer.pop()).toEqual(10);
+    expect(buffer.pop()).toEqual(0);
 });
+
--- ring_buffer/ring_buffer.ts
+++ ring_buffer/ring_buffer.ts
@@ -1,18 +1,20 @@
 export default class ring_buffer<T> {
     private buffer: (T | undefined)[];
-    private tail: number;
+    private tail?: number;
     private head: number;
 
     constructor() {
+        this.buffer = new Array<T>(3);
         this.head = 0;
-        this.tail = 0;
-        this.buffer = new Array<T>(5);
     }
 
     push(item: T): (T | undefined)[] {
         //if(this.tail === this.head) {
         // TODO: add a while loop for making a new bigger array;
         //}
+        if (!this.tail) {
+            this.tail = 0;
+        }
         const tail = this.tail % this.buffer.length;
         this.buffer[tail] = item;
         this.tail++;
@@ -20,15 +22,27 @@ export default class ring_buffer<T> {
     }
 
     pop(): T | undefined {
+        if(!this.tail) {
+            return undefined;
+        }
+
         const head = this.head % this.buffer.length;
         const deleted_value = this.buffer[head];
+
+        if(this.buffer[head] === undefined) {
+            return undefined;
+        }
+
         this.buffer[head] = undefined;
         this.head++;
         return deleted_value; 
     }
 
-
     get(index: number): T | undefined {
         return this.buffer[index];
+    }
+    
+    get_buffer(): (T | undefined)[] {
+        return this.buffer;
     }
 }
